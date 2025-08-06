acf414ebd9ceca620ebf38c4ced16bfaafe58b0a
founders <ser.razum2003@gmail.com>
2025-08-06 23:12:48 +0400

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,31 @@
 import {NavLink} from "react-router-dom";
+import SidebarMenuMobil from "./SidebarMenuMobil.jsx";
+import {useState} from "react";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className={"header"}>
-
             <div className={"header__mobil"}>
-                <NavLink className={"link__logo"} to={"/"}>OK</NavLink>
+                <div className={"header__menu__logo"}>
+                    <button onClick={() => setIsOpen(!isOpen)} className={"menuBurder"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                        </svg>
+                    </button>
+                    <NavLink className={"link__logo"} to={"/"}>OK</NavLink>
+                </div>
                 <form className={"header__mobil-form"}>
-                        <div className={"group"}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" className="search__icon"
-                                 viewBox="0 0 16 16">
+                    <div className={"group"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="15" fill="currentColor" className="search__icon"
+                             viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                            </svg>
-                            <input className={"grop__input"} placeholder={"Поиск"}/>
-                        </div>
-                    </form>
+                        </svg>
+                        <input className={"grop__input"} placeholder={"Поиск"}/>
+                    </div>
+                </form>
+                {isOpen && (<SidebarMenuMobil/>)}
             </div>
         </header>
     )
---
+++ src/components/SidebarMenuMobil.jsx
@@ -0,0 +1,14 @@
+import {NavLink} from "react-router-dom"
+
+function SidebarMenuMobil() {
+    return (
+        <nav className={"sidebar__menu"}>
+            <ul className={"menu__menu__list"}>
+                <li className={"menu__menu__list-item"}><NavLink to={"/popular"}>Популярное</NavLink> </li>
+                <li className={"menu__menu__list-item"}><NavLink to={"/recommendations"}>Рекомендации</NavLink></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default SidebarMenuMobil;
